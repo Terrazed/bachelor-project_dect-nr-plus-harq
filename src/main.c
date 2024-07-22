7187b12cadc19213da71ad4ab8424b0e66719df3
Terrazed <95218539+Terrazed@users.noreply.github.com>
2024-07-22 14:33:11 +0200

    added dect_mac_phy_handler_cb.h

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <zephyr/kernel.h>
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main);
+
 int main(void)
 {
+        LOG_INF("Starting main");
+
+        
+
+
         return 0;
 }
