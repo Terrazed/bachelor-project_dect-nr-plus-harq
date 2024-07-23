c73ab34620390936f1ea2c0c7eccdbd9b93c4b24
Terrazed <95218539+Terrazed@users.noreply.github.com>
2024-07-23 15:29:42 +0200

    coded the tx function
    not tested

--- src/dect_mac_phy_handler.c
+++ src/dect_mac_phy_handler.c
@@ -4,6 +4,7 @@
 LOG_MODULE_REGISTER(handler);
 
 K_SEM_DEFINE(phy_access_sem, 1, 1);
+uint16_t device_id = 0;
 
 int dect_mac_phy_handler_start_modem(){
 
@@ -16,12 +17,17 @@ int dect_mac_phy_handler_start_modem(){
         return ret;
     }
 
+
+
     /* set the callbacks for the dect phy modem */
     ret = nrf_modem_dect_phy_callback_set(dect_mac_phy_handler_get_callbacks());
     if(ret){
         LOG_ERR("nrf_modem_dect_phy_callback_set() returned %d", ret);
         return ret;
     }
+
+    /* Get the device ID. */
+    hwinfo_get_device_id((void *)&device_id, sizeof(device_id));
 
     /* get the capability of the api */
     dect_mac_phy_handler_capability_get();
@@ -128,9 +134,17 @@ void dect_mac_phy_handler_rx(struct dect_mac_phy_handler_rx_params params){
 }
 
 
-void dect_mac_phy_handler_tx(){
+void dect_mac_phy_handler_tx(struct dect_phy_handler_tx_params params){
 
+    /* create true params */
+    DECT_MAC_PHY_HANDLER_TRUE_PARAM_CREATE(true_params, params);
     
+    /* start the transmission */
+    int ret = nrf_modem_dect_phy_tx(&true_params);
+    if(ret){
+        LOG_ERR("nrf_modem_dect_phy_tx() returned %d", ret);
+    }
+
 }
 
 
@@ -169,3 +183,77 @@ void dect_mac_phy_handler_time_get(){
     
 }
 
+void dect_mac_phy_handler_tx_config(struct dect_phy_handler_tx_params *input_params, struct nrf_modem_dect_phy_tx_params *output_params){
+
+    /* set the time */
+    output_params->start_time = input_params->start_time;
+
+    /* set the handle */
+    output_params->handle = ((TX << 28) | (input_params->handle & 0x0fffffff));
+
+    /* set the network id */
+    output_params->network_id = CONFIG_NETWORK_ID;
+
+    /* setup lbt */
+    output_params->lbt_rssi_threshold_max = CONFIG_RSSI_TARGET;
+    output_params->lbt_period = input_params->lbt_enable ? NRF_MODEM_DECT_LBT_PERIOD_MAX : 0;
+
+    /* set the carrier */
+    output_params->carrier = CONFIG_CARRIER;
+
+    uint32_t tx_power = 11; //TODO: create a function to calculate these
+    uint32_t df_mcs = 2;
+
+    uint32_t packet_length_type = 0; //TODO: create a function to calculate these
+    uint32_t packet_length = 0;
+
+    if(input_params->tx_usage == BEACON){
+        output_params->phy_type = HEADER_TYPE_1;
+        struct phy_ctrl_field_common_type1 *header = (struct phy_ctrl_field_common_type1 *)output_params->phy_header;
+
+        header->header_format = HEADER_FORMAT_000;
+        header->packet_length_type = packet_length_type;
+        header->packet_length = packet_length;
+        header->short_network_id = (CONFIG_NETWORK_ID & 0xff);
+        header->transmitter_id_hi = (device_id >> 8);
+        header->transmitter_id_lo = (device_id & 0xff);
+        header->transmit_power = tx_power; //TODO: set to the worst connection in the registered nodes
+        header->reserved = 0;
+        header->df_mcs = df_mcs; //TODO: set to the worst connection in the registered nodes
+    }
+    else{
+        output_params->phy_type = HEADER_TYPE_2;
+        struct phy_ctrl_field_common_type2 *header = (struct phy_ctrl_field_common_type2 *)output_params->phy_header;
+
+        header->header_format = HEADER_FORMAT_001;
+        header->packet_length_type = packet_length_type;
+        header->packet_length = packet_length;
+        header->short_network_id = (CONFIG_NETWORK_ID & 0xff);
+        header->transmitter_id_hi = (device_id >> 8);
+        header->transmitter_id_lo = (device_id & 0xff);
+        header->transmit_power = tx_power;
+        header->df_mcs = df_mcs;
+        header->receiver_id_hi = (input_params->receiver_id >> 8);
+        header->receiver_id_lo = (input_params->receiver_id & 0xff);
+        header->spatial_streams = 0; // device does not support spatial streams
+        header->feedback_format = input_params->feedback.format;
+        header->feedback_info_hi = input_params->feedback.info.byte.hi;
+        header->feedback_info_lo = input_params->feedback.info.byte.lo;
+
+        if(input_params->tx_usage == HARQ){
+            header->df_red_version = input_params->harq.redundancy_version;
+            header->df_ind = input_params->harq.new_data_indication;
+            header->df_harq_process_nr = input_params->harq.harq_process_nr;
+        }
+        else{
+            header->df_red_version = 0;
+            header->df_ind = 0;
+            header->df_harq_process_nr = 0;
+        }
+    }
+
+    output_params->bs_cqi = NRF_MODEM_DECT_PHY_BS_CQI_NOT_USED; //TODO: implement cqi and buffer
+    output_params->data = input_params->data;
+    output_params->data_size = input_params->data_size;
+
+}
--- src/dect_mac_phy_handler.h
+++ src/dect_mac_phy_handler.h
@@ -4,30 +4,10 @@
 #include <zephyr/kernel.h>
 #include <nrf_modem_dect_phy.h>
 
+#include "dect_mac_phy_handler_types.h"
 #include "dect_mac_phy_handler_cb.h"
 
-enum dect_mac_phy_function {
-    CAPABILITY_GET = 1,
-    INIT = 2,
-    DEINIT = 3,
-    RX = 4,
-    TX = 5,
-    TX_HARQ = 6,
-    TX_RX = 7,
-    RSSI = 8,
-    RX_STOP = 9,
-    LINK_CONFIG = 10,
-    TIME_GET = 11,
-};
 
-
-struct dect_mac_phy_handler_rx_params {
-    uint32_t handle : 28;
-    enum nrf_modem_dect_phy_rx_mode rx_mode;
-    uint32_t rx_period_ms;
-    uint16_t receiver_identity;
-    uint64_t start_time;
-};
 
 /* functions to start and stop the modem, the modem should be started when using the other commands */
 int dect_mac_phy_handler_start_modem();
@@ -38,7 +18,7 @@ void dect_mac_phy_handler_capability_get();
 void dect_mac_phy_handler_init();
 void dect_mac_phy_handler_deinit();
 void dect_mac_phy_handler_rx(struct dect_mac_phy_handler_rx_params params);
-void dect_mac_phy_handler_tx();
+void dect_mac_phy_handler_tx(struct dect_phy_handler_tx_params params);
 void dect_mac_phy_handler_tx_harq();
 void dect_mac_phy_handler_tx_rx();
 void dect_mac_phy_handler_rssi();
@@ -46,9 +26,15 @@ void dect_mac_phy_handler_rx_stop();
 void dect_mac_phy_handler_link_config();
 void dect_mac_phy_handler_time_get();
 
+#define DECT_MAC_PHY_HANDLER_TRUE_PARAM_CREATE(name, phy_handler_param) struct nrf_modem_dect_phy_tx_params name;union nrf_modem_dect_phy_hdr header;name.phy_header = &header;dect_mac_phy_handler_tx_config(&phy_handler_param, &name);
+void dect_mac_phy_handler_tx_config(struct dect_phy_handler_tx_params *input_params, struct nrf_modem_dect_phy_tx_params *output_params);
+
 
 /* variable that holds the capability of the modem (declared in dect_mac_phy_handler_cb.c) */
 extern struct dect_capabilities capabilities;
+
+/* variable that holds the device id of the device (declared in dect_mac_phy_handler.c) */
+extern uint16_t device_id;
 
 /* semaphore to protect the access to the phy layer api (declared in dect_mac_phy_handler.c)*/
 extern struct k_sem phy_access_sem;
