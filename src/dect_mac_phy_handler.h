ab4f5e437b3a9edbabcabc268676ef24b60bd2eb
Terrazed <95218539+Terrazed@users.noreply.github.com>
2024-07-23 09:30:16 +0200

    added little commentsin dect_mac_phy_handler.h

--- src/dect_mac_phy_handler.h
+++ src/dect_mac_phy_handler.h
@@ -4,9 +4,11 @@
 #include <zephyr/kernel.h>
 #include <nrf_modem_dect_phy.h>
 
+/* functions to start and stop the modem, the modem should be started when using the other commands */
 int dect_mac_phy_handler_start_modem();
 int dect_mac_phy_handler_stop_modem();
 
+/* functions that simplify the access to the phy layer api */
 void dect_mac_phy_handler_capability_get();
 void dect_mac_phy_handler_init();
 void dect_mac_phy_handler_deinit();
