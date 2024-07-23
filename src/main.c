6855cef599c5d2e818734d12740472c9f70794c2
Terrazed <95218539+Terrazed@users.noreply.github.com>
2024-07-23 11:06:28 +0200

    little test for the start_modem function

--- src/main.c
+++ src/main.c
@@ -1,14 +1,28 @@
 #include <zephyr/kernel.h>
 
+#include <nrf_modem_dect_phy.h>
+#include <modem/nrf_modem_lib.h>
+
+#include "dect_mac_phy_handler.h"
+
+
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main);
+LOG_MODULE_REGISTER(main,4);
 
 int main(void)
 {
-        LOG_INF("Starting main");
+        LOG_DBG("Starting main");
+
+        int err;
+        err = dect_mac_phy_handler_start_modem();
+        if(err){
+            LOG_ERR("Failed to start modem");
+            return err;
+        }
+
+        LOG_DBG("Modem started");
 
         
-
 
         return 0;
 }
