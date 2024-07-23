9fd289f5d572de0c617820c23d3863d47e4577b0
Terrazed <95218539+Terrazed@users.noreply.github.com>
2024-07-23 09:34:14 +0200

    Create dect_mac_phy_handler.c

---
+++ src/dect_mac_phy_handler.c
@@ -0,0 +1,79 @@
+#include "dect_mac_phy_handler.h"
+
+int dect_mac_phy_handler_start_modem(){
+
+    
+}
+
+
+int dect_mac_phy_handler_stop_modem(){
+
+    
+}
+
+
+void dect_mac_phy_handler_capability_get(){
+
+    
+}
+
+
+void dect_mac_phy_handler_init(){
+
+    
+}
+
+
+void dect_mac_phy_handler_deinit(){
+
+    
+}
+
+
+void dect_mac_phy_handler_rx(){
+
+    
+}
+
+
+void dect_mac_phy_handler_tx(){
+
+    
+}
+
+
+void dect_mac_phy_handler_tx_harq(){
+
+    
+}
+
+
+void dect_mac_phy_handler_tx_rx(){
+
+    
+}
+
+
+void dect_mac_phy_handler_rssi(){
+
+    
+}
+
+
+void dect_mac_phy_handler_rx_stop(){
+
+    
+}
+
+
+void dect_mac_phy_handler_link_config(){
+
+    
+}
+
+
+void dect_mac_phy_handler_time_get(){
+
+    
+}
+
