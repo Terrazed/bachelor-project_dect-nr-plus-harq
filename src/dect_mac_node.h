70c1aeab4a6af5368fb62f108153c0f84525723e
Terrazed <95218539+Terrazed@users.noreply.github.com>
2024-07-30 14:57:31 +0200

    change outputs

--- src/dect_mac_node.h
+++ src/dect_mac_node.h
@@ -37,36 +37,36 @@ void dect_mac_node_thread();
 void dect_mac_node_clean_unused_nodes();
 
 /* function that get the tx power of a node */
-uint32_t dect_mac_node_get_tx_power(uint32_t address);
+int dect_mac_node_get_tx_power(uint32_t address);
 
 /* function that get the mcs of a node */
-uint32_t dect_mac_node_get_mcs(uint32_t address);
+int dect_mac_node_get_mcs(uint32_t address);
 
 /* function that create a node and put it in the hashmap */
-bool dect_mac_node_create_node(uint32_t address, uint32_t tx_power, uint32_t mcs);
+int dect_mac_node_create_node(uint32_t address, uint32_t tx_power, uint32_t mcs);
 
 /* function that delete a node from the hashmap */
-bool dect_mac_node_delete_node(uint32_t address);
+int dect_mac_node_delete_node(uint32_t address);
 
 /* function that check if a node is in the hashmap */
-bool dect_mac_node_contains_node(uint32_t address);
+int dect_mac_node_contains_node(uint32_t address);
 
 /* function that set the tx power of a node */
-bool dect_mac_node_set_tx_power(uint32_t address, uint32_t tx_power);
+int dect_mac_node_set_tx_power(uint32_t address, uint32_t tx_power);
 
 /* function that set the mcs of a node */
-bool dect_mac_node_set_mcs(uint32_t address, uint32_t mcs);
+int dect_mac_node_set_mcs(uint32_t address, uint32_t mcs);
 
 /* function that optimize the transmission power and modulation to the lowest while still ensuring communication. */
-bool dect_mac_node_optimize(uint32_t address, int32_t rssi2, int32_t snr, uint32_t received_tx_power, uint32_t received_mcs);
+int dect_mac_node_optimize(uint32_t address, int32_t rssi2, int32_t snr, uint32_t received_tx_power, uint32_t received_mcs);
 
 /* function that add power to a node */
-bool dect_mac_node_add_power(uint32_t address, int32_t power_to_add);
+int dect_mac_node_add_power(uint32_t address, int32_t power_to_add);
 
 /* function that reduce the mcs level of a node */
-bool dect_mac_node_reduce_mcs(uint32_t address, int32_t mcs_to_reduce);
+int dect_mac_node_reduce_mcs(uint32_t address, int32_t mcs_to_reduce);
 
 /* function that maxes the power of a node */
-bool dect_mac_node_full_power(uint32_t address);
+int dect_mac_node_full_power(uint32_t address);
 
 #endif // DECT_MAC_NODE
