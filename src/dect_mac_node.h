e2c63177fff86bf073cca2d168dcb566fbddd51a
Terrazed <95218539+Terrazed@users.noreply.github.com>
2024-07-30 14:53:53 +0200

    Create dect_mac_node.h

---
+++ src/dect_mac_node.h
@@ -0,0 +1,72 @@
+#ifndef DECT_MAC_NODE
+#define DECT_MAC_NODE
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/hash_map.h>
+
+/* define the minimum time that a node will kept if not used 
+(the node will be deleted after two cycle so it can 
+take up to double the time with a average at 1.5 times the time) */
+#define DECT_MAC_NODE_DELETE_TIMEOUT K_MINUTES(10)
+
+/* struct that represent a node */
+struct node{
+    uint32_t address;
+    bool used;
+    int32_t tx_power;
+    int32_t mcs;
+    uint32_t buffer_size;
+};
+
+
+
+/* hashmap that contains all the nodes, the keys are the nodes addresses */
+extern struct sys_hashmap node_hashmap;
+
+/* mutex to protect the node_hashmap */
+extern struct k_mutex node_mutex;
+
+
+
+/* thread that handle the deletion of the not used nodes */
+void dect_mac_node_thread();
+
+
+
+/* function that cleans all the nodes not used in the past #DECT_MAC_NODE_DELETE_TIMEOUT */
+void dect_mac_node_clean_unused_nodes();
+
+/* function that get the tx power of a node */
+uint32_t dect_mac_node_get_tx_power(uint32_t address);
+
+/* function that get the mcs of a node */
+uint32_t dect_mac_node_get_mcs(uint32_t address);
+
+/* function that create a node and put it in the hashmap */
+bool dect_mac_node_create_node(uint32_t address, uint32_t tx_power, uint32_t mcs);
+
+/* function that delete a node from the hashmap */
+bool dect_mac_node_delete_node(uint32_t address);
+
+/* function that check if a node is in the hashmap */
+bool dect_mac_node_contains_node(uint32_t address);
+
+/* function that set the tx power of a node */
+bool dect_mac_node_set_tx_power(uint32_t address, uint32_t tx_power);
+
+/* function that set the mcs of a node */
+bool dect_mac_node_set_mcs(uint32_t address, uint32_t mcs);
+
+/* function that optimize the transmission power and modulation to the lowest while still ensuring communication. */
+bool dect_mac_node_optimize(uint32_t address, int32_t rssi2, int32_t snr, uint32_t received_tx_power, uint32_t received_mcs);
+
+/* function that add power to a node */
+bool dect_mac_node_add_power(uint32_t address, int32_t power_to_add);
+
+/* function that reduce the mcs level of a node */
+bool dect_mac_node_reduce_mcs(uint32_t address, int32_t mcs_to_reduce);
+
+/* function that maxes the power of a node */
+bool dect_mac_node_full_power(uint32_t address);
+
+#endif // DECT_MAC_NODE
