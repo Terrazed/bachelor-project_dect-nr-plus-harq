bf6366fb997c09cb2398cf1f2eef6aba6d8d6126
Terrazed <95218539+Terrazed@users.noreply.github.com>
2024-08-02 10:16:21 +0200

    reformat document

--- src/dect_mac_phy_handler_queue.c
+++ src/dect_mac_phy_handler_queue.c
@@ -1,7 +1,7 @@
 #include "dect_mac_phy_handler_queue.h"
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(phy_queue,3);
+LOG_MODULE_REGISTER(phy_queue, 3);
 
 K_THREAD_DEFINE(dect_phy_queue_handler_exec_thread_id, DECT_MAC_PHY_HANDLER_QUEUE_THREAD_STACK_SIZE, dect_mac_phy_handler_queue_exec_thread, NULL, NULL, NULL, 10, 0, 0);
 K_SEM_DEFINE(phy_layer_sem, 1, 1);
@@ -9,7 +9,7 @@ K_SEM_DEFINE(queue_item_sem, 0, DECT_MAC_PHY_HANDLER_QUEUE_MAX_ITEMS);
 K_MUTEX_DEFINE(queue_mutex);
 K_TIMER_DEFINE(dect_mac_phy_handler_queue_operation_failed_timer, dect_mac_phy_handler_queue_timer_callback, NULL);
 K_THREAD_DEFINE(dect_phy_queue_handler_put_thread_id, DECT_MAC_PHY_HANDLER_QUEUE_THREAD_STACK_SIZE, dect_mac_phy_handler_queue_put_thread, NULL, NULL, NULL, 9, 0, 0);
-K_MSGQ_DEFINE(dect_mac_phy_handler_queue_msgq, sizeof(struct dect_mac_phy_handler_queue_item*), DECT_MAC_PHY_HANDLER_QUEUE_MAX_ITEMS, 1);
+K_MSGQ_DEFINE(dect_mac_phy_handler_queue_msgq, sizeof(struct dect_mac_phy_handler_queue_item *), DECT_MAC_PHY_HANDLER_QUEUE_MAX_ITEMS, 1);
 
 sys_slist_t dect_mac_phy_handler_queue = SYS_SLIST_STATIC_INIT(&dect_mac_phy_handler_queue);
 struct dect_mac_phy_handler_queue_node current_node = {0};
@@ -24,31 +24,29 @@ int dect_phy_queue_put(enum dect_mac_phy_function function, union dect_mac_phy_h
     if (dect_mac_phy_handler_queue_item == NULL)
     {
         LOG_ERR("Failed to allocate memory for the work");
-        return -1; //TODO: return a proper error code
+        return -1; // TODO: return a proper error code
     }
-    
 
     /* create the work context */
     dect_mac_phy_handler_queue_item->function = function;
-    if(params != NULL)
+    if (params != NULL)
     {
         dect_mac_phy_handler_queue_item->params = *params;
-    } 
+    }
     else
     {
         dect_mac_phy_handler_queue_item->params = (union dect_mac_phy_handler_params){0};
     }
     dect_mac_phy_handler_queue_item->priority = priority;
 
-
     LOG_DBG("item: %p, function: %d, params: %x, priority: %u", dect_mac_phy_handler_queue_item, function, params, priority);
 
     /* submit the request */
     int ret = k_msgq_put(&dect_mac_phy_handler_queue_msgq, &dect_mac_phy_handler_queue_item, K_FOREVER);
-    if(ret)
+    if (ret)
     {
         LOG_ERR("Failed to put the item in the queue");
-        return -1; //TODO: return a proper error code
+        return -1; // TODO: return a proper error code
     }
 
     return 0;
@@ -56,12 +54,12 @@ int dect_phy_queue_put(enum dect_mac_phy_function function, union dect_mac_phy_h
 
 void dect_mac_phy_handler_queue_put_thread(struct k_work *work)
 {
-    while(1)
+    while (1)
     {
         /* get the item */
         struct dect_mac_phy_handler_queue_item *dect_mac_phy_handler_queue_item;
         int ret = k_msgq_get(&dect_mac_phy_handler_queue_msgq, &dect_mac_phy_handler_queue_item, K_FOREVER);
-        if(ret)
+        if (ret)
         {
             LOG_ERR("Failed to get the item from the queue");
         }
@@ -100,7 +98,7 @@ void dect_mac_phy_handler_queue_put_thread(struct k_work *work)
 
             /* check if the maximum number of node in the queue is already reached */
             if (sys_slist_len(&dect_mac_phy_handler_queue) >= DECT_MAC_PHY_HANDLER_QUEUE_MAX_ITEMS) // warning! this operation is O(n) which means that if the queue max item is big it should probably be handled differently
-            { 
+            {
                 LOG_ERR("The queue is full, cannot add more items");
                 k_mutex_unlock(&queue_mutex);
                 return;
@@ -114,12 +112,12 @@ void dect_mac_phy_handler_queue_put_thread(struct k_work *work)
             /* if the list is not empty, add the item to the correct position */
             else
             {
-                bool node_inserted = false; // flag to check if the item was inserted
-                sys_snode_t *previous_node = NULL; // previous node to insert the item
+                bool node_inserted = false;                                                    // flag to check if the item was inserted
+                sys_snode_t *previous_node = NULL;                                             // previous node to insert the item
                 for (sys_snode_t *sys_node = sys_slist_peek_head(&dect_mac_phy_handler_queue); // create a node to iterate through the list
-                    sys_node != NULL;                                                         // iterate until the end of the list (but will break if the item is inserted before the end of the list)
-                    previous_node = sys_node, sys_node = sys_slist_peek_next(sys_node))               // set the previous node to the current node and the current node to the next node
-                { 
+                     sys_node != NULL;                                                         // iterate until the end of the list (but will break if the item is inserted before the end of the list)
+                     previous_node = sys_node, sys_node = sys_slist_peek_next(sys_node))       // set the previous node to the current node and the current node to the next node
+                {
                     /* get the current node */
                     struct dect_mac_phy_handler_queue_node *current_node = CONTAINER_OF(sys_node, struct dect_mac_phy_handler_queue_node, node);
                     /* if the current item has a lower priority than the item to insert, insert the item before the current item */
@@ -151,61 +149,61 @@ int dect_mac_phy_handler_queue_function_execute(enum dect_mac_phy_function funct
     current_node.item.params = *params;
 
     /* execute the operation */
-    switch(function)
+    switch (function)
     {
-        case CAPABILITY_GET:
-            LOG_DBG("capability get");
-            dect_mac_phy_handler_capability_get();
-            break;
-        case INIT:
-            LOG_DBG("init");
-            dect_mac_phy_handler_init();
-            break;
-        case DEINIT:
-            LOG_DBG("deinit");
-            dect_mac_phy_handler_deinit();
-            break;
-        case RX:
-            LOG_DBG("rx");
-            params->rx_params.handle |= (1<<27); // set in the handle that the operation comes from the queue
-            dect_mac_phy_handler_rx(params->rx_params);
-            break;
-        case TX:
-            LOG_DBG("tx");
-            params->tx_params.handle |= (1<<27); // set in the handle that the operation comes from the queue
-            dect_mac_phy_handler_tx(params->tx_params);
-            break;
-        case TX_HARQ:
-            LOG_DBG("tx harq");
-            params->tx_harq_params.handle |= (1<<27); // set in the handle that the operation comes from the queue
-            dect_mac_phy_handler_tx_harq(params->tx_harq_params);
-            break;
-        case TX_RX:
-            LOG_DBG("tx rx");
-            params->tx_rx_params.handle |= (1<<27); // set in the handle that the operation comes from the queue
-            dect_mac_phy_handler_tx_rx(params->tx_rx_params);
-            break;
-        case RSSI:
-            LOG_DBG("rssi");
-            dect_mac_phy_handler_rssi(params->rssi_params);
-            break;
-        case RX_STOP:
-            LOG_DBG("rx stop");
-            dect_mac_phy_handler_rx_stop(params->rx_stop_params);
-            break;
-        case LINK_CONFIG:
-            LOG_DBG("link config");
-            dect_mac_phy_handler_link_config();
-            break;
-        case TIME_GET:
-            LOG_DBG("time get");
-            dect_mac_phy_handler_time_get();
-            break;
-        case PLACEHOLDER:
-            break;
-        default:
-            LOG_ERR("Unknown function: %d", function);
-            return -1; // TODO: return a proper error code
+    case CAPABILITY_GET:
+        LOG_DBG("capability get");
+        dect_mac_phy_handler_capability_get();
+        break;
+    case INIT:
+        LOG_DBG("init");
+        dect_mac_phy_handler_init();
+        break;
+    case DEINIT:
+        LOG_DBG("deinit");
+        dect_mac_phy_handler_deinit();
+        break;
+    case RX:
+        LOG_DBG("rx");
+        params->rx_params.handle |= (1 << 27); // set in the handle that the operation comes from the queue
+        dect_mac_phy_handler_rx(params->rx_params);
+        break;
+    case TX:
+        LOG_DBG("tx");
+        params->tx_params.handle |= (1 << 27); // set in the handle that the operation comes from the queue
+        dect_mac_phy_handler_tx(params->tx_params);
+        break;
+    case TX_HARQ:
+        LOG_DBG("tx harq");
+        params->tx_harq_params.handle |= (1 << 27); // set in the handle that the operation comes from the queue
+        dect_mac_phy_handler_tx_harq(params->tx_harq_params);
+        break;
+    case TX_RX:
+        LOG_DBG("tx rx");
+        params->tx_rx_params.handle |= (1 << 27); // set in the handle that the operation comes from the queue
+        dect_mac_phy_handler_tx_rx(params->tx_rx_params);
+        break;
+    case RSSI:
+        LOG_DBG("rssi");
+        dect_mac_phy_handler_rssi(params->rssi_params);
+        break;
+    case RX_STOP:
+        LOG_DBG("rx stop");
+        dect_mac_phy_handler_rx_stop(params->rx_stop_params);
+        break;
+    case LINK_CONFIG:
+        LOG_DBG("link config");
+        dect_mac_phy_handler_link_config();
+        break;
+    case TIME_GET:
+        LOG_DBG("time get");
+        dect_mac_phy_handler_time_get();
+        break;
+    case PLACEHOLDER:
+        break;
+    default:
+        LOG_ERR("Unknown function: %d", function);
+        return -1; // TODO: return a proper error code
     }
     return 0;
 }
@@ -265,9 +263,9 @@ void dect_mac_phy_handler_queue_operation_failed_retry(struct k_timer *timer)
 
     /* increment the counter */
     dect_mac_phy_handler_queue_operation_failed_counter++;
-    
+
     /* check if over the operation max retry limit */
-    if(dect_mac_phy_handler_queue_operation_failed_counter > DECT_MAC_PHY_HANDLER_QUEUE_MAX_RETRY)
+    if (dect_mac_phy_handler_queue_operation_failed_counter > DECT_MAC_PHY_HANDLER_QUEUE_MAX_RETRY)
     {
         LOG_WRN("Operation failed too many times, aborting");
         k_sem_give(&phy_layer_sem);
@@ -276,7 +274,6 @@ void dect_mac_phy_handler_queue_operation_failed_retry(struct k_timer *timer)
     {
         k_timer_start(&dect_mac_phy_handler_queue_operation_failed_timer, DECT_MAC_PHY_HANDLER_QUEUE_RETRY_DELAY, K_NO_WAIT);
     }
-
 }
 
 void dect_mac_phy_handler_queue_timer_callback(struct k_timer *timer_id)
